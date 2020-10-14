366cb78325519c542e3a65936f02020cf8e81e9c
Noskov Alexey <vokson@yandex.ru>
2020-10-14 20:04:46 +0300

    Update SettingSeeder.php

--- database/seeds/SettingSeeder.php
+++ database/seeds/SettingSeeder.php
@@ -24,6 +24,7 @@ class SettingSeeder extends Seeder
             'RATING_INITIAL_VALUE' => '0.5',
             'LAST_TASK_NUMBER' => '001',
             'CREATE_TASK_NOTIFICATION' => 'Создан TASK',
+            // Одинарное экранирование, так как это PHP регулярное выражение
             'CHECKER_REG_EXP_FOR_NEW_FILE' => '/^4022-[A-Z]{2}-[A-Z]{2}-(00000|66210|66220|66230|66321|66340|66341|66422|66450|66560|66570|66580|66690)(\d{4}|\d{2}-\d{3}|\d{1}-\d{3}(\s[A-Z]\d?)?)-IS\d{1}[A-Z0-9]{1}(_R)?(_C_RH_(OP|CL))?\.(dwg|DWG|pdf|PDF|doc|DOC|docx|DOCX|xls|XLS|xlsx|XLSX|zip|ZIP){1}$/',
             'CHECKER_REG_EXP_FOR_CHECKED_FILE' => '/^4022-[A-Z]{2}-[A-Z]{2}-(00000|66210|66220|66230|66321|66340|66341|66422|66450|66560|66570|66580|66690)(\d{4}|\d{2}-\d{3}|\d{1}-\d{3}(\s[A-Z]\d?)?)-IS\d{1}[A-Z0-9]{1}(_R)?(_C_RH_(OP|CL))?\[\d+\]\.(dwg|DWG|pdf|PDF|doc|DOC|docx|DOCX|xls|XLS|xlsx|XLSX|zip|ZIP){1}$/',
             'DOCS_REG_EXP_FOR_LIST_FILE' => '/.+\.json$/',
@@ -31,15 +32,18 @@ class SettingSeeder extends Seeder
             'DOCS_REG_EXP_FOR_PDF_FILE' => '/.*(pdf|PDF)$/',
             'STATUS_ID_FOR_NEW_TRANSMITTAL' => Status::where('name', 'TRANSMITTAL')->first()->id,
             'SENDER_FOLDER_NOTIFICATION_SUBJECT' => 'ВОЛГАФЕРТ. ОТПРАВКА.',
+            'FRONTEND_PROJECT_NAME_RU' => 'Проектный офис ВОЛГАФЕРТ',
+            'FRONTEND_PROJECT_NAME_EN' => 'Design office VOLGAFERT',
             'FRONTEND_CODE_1_NAME_RU' => 'КОД ДОКУМЕНТА 1',
             'FRONTEND_CODE_1_NAME_EN' => 'DOC CODE 1',
             'FRONTEND_CODE_2_NAME_RU' => 'КОД ДОКУМЕНТА 2',
             'FRONTEND_CODE_2_NAME_EN' => 'DOC CODE 2',
-            'FRONTEND_DOCS_CODE_1_REG_EXP' => '^4022-[A-Z]{2}-[A-Z]{2}-(00000|66210|66220|66230|66321|66340|66341|66422|66450|66560|66570|66580|66690)(\\d{4}|\\d{2}-\\d{3}|\\d{1}-\\d{3}(\\s[A-Z]\\d?)?)(_C_RH_(OP|CL))?$',
-            'FRONTEND_DOCS_CODE_2_REG_EXP' => '^7500081106-(00000|66210|66220|66230|66321|66340|66341|66422|66450|66560|66570|66580|66690)-(КМ|КЖ|АР|ЭГ|НВК|НВК|ОВ)\\d{0,2}(\\.(РР|ТИ|ТЗ))?-\\d{4}$',
-            'FRONTEND_DOCS_REV_REG_EXP' => '^\\d{1}[A-Z0-9]{1}$',
-            'FRONTEND_DOCS_CLASS_REG_EXP' => '^(A|C|I|RQ|FI|IFC)$',
-            'FRONTEND_RECORDS_ADD_WORDS_TO_TEXT' => 'TQ пришел в|Отчет в ревизии отправлен в|Комментарии к отчету в ревизии пришли в',
+            // Двойное экранирование, так как это уже JS регулярное выражение
+            'FRONTEND_DOCS_CODE_1_JS_REG_EXP' => '^4022-[A-Z]{2}-[A-Z]{2}-(00000|66210|66220|66230|66321|66340|66341|66422|66450|66560|66570|66580|66690)(\\d{4}|\\d{2}-\\d{3}|\\d{1}-\\d{3}(\\s[A-Z]\\d?)?)(_C_RH_(OP|CL))?$',
+            'FRONTEND_DOCS_CODE_2_JS_REG_EXP' => '^7500081106-(00000|66210|66220|66230|66321|66340|66341|66422|66450|66560|66570|66580|66690)-(КМ|КЖ|АР|ЭГ|НВК|НВК|ОВ)\\d{0,2}(\\.(РР|ТИ|ТЗ))?-\\d{4}$',
+            'FRONTEND_DOCS_REV_JS_REG_EXP' => '^\\d{1}[A-Z0-9]{1}$',
+            'FRONTEND_DOCS_CLASS_JS_REG_EXP' => '^(A|C|I|RQ|FI|IFC)$',
+            'FRONTEND_RECORDS_ADD_WORDS_TO_TEXT_IN_JSON' => '["Здесь должны","быть слова","которые можно добавить"]',
             'FRONTEND_TITLES_FIELD_1_NAME_RU' => 'Предшественник',
             'FRONTEND_TITLES_FIELD_1_NAME_EN' => 'Predecessor',
             'FRONTEND_TITLES_FIELD_2_NAME_RU' => 'Описание',
@@ -51,6 +55,8 @@ class SettingSeeder extends Seeder
             'FRONTEND_CHECKER_MAX_FILE_SIZE_MB' => '50',
             'FRONTEND_SENDER_MAX_FILE_SIZE_MB' => '1000',
             'FRONTEND_MERGE_PDF_MAX_FILE_SIZE_MB' => '100',
+            // Четверное экранирование, так как сначала парсится JSON, а потом регулярное выражение передается в PHP
+            'FRONTEND_CHARTS_TITLES_PHP_REG_EXP_IN_JSON' => '[{"key": "All / Все", "value": "/.*/"}, {"key": "KJ*.DRW", "value": "/.*[-]{1}KJ[\\\\d]*\\\\.DRW$/"}]',
         ];
 
         foreach ($settings as $key => $value) {
